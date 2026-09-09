a0bbf7d679e98196de6886e806cea890fe544053
hamed <amzhamed@gmail.com>
2026-09-09 15:30:30 -0700

    test

---
+++ marker_grid_controller/led.py
@@ -0,0 +1,100 @@
+import argparse
+import sys
+import time
+import board
+import neopixel_spi as neopixel
+
+
+class LED:
+    def __init__(self, num_pixels=50, color=(227, 253, 255), tail_decay=0.75, delay=0.02, brightness=1.0):
+        self.num_pixels = num_pixels
+        self.color = color
+        self.tail_decay = tail_decay
+        self.delay = delay
+        self.leds = [(0, 0, 0)] * self.num_pixels
+        self.running = False
+
+        # Setup SPI and NeoPixel strip
+        self.pixels = neopixel.NeoPixel_SPI(
+            board.SPI(),
+            self.num_pixels,
+            pixel_order=neopixel.GRB,
+            auto_write=False,
+            brightness=brightness
+        )
+
+    def fade_tail(self):
+        for i in range(self.num_pixels):
+            r, g, b = self.leds[i]
+            self.leds[i] = (
+                int(r * self.tail_decay),
+                int(g * self.tail_decay),
+                int(b * self.tail_decay)
+            )
+
+    def draw_frame(self):
+        for i, color in enumerate(self.leds):
+            self.pixels[i] = [int(c) for c in color]
+        self.pixels.show()
+
+    def turn_on(self):
+        # self.leds[0] = self.color
+        self.leds[11] = self.color
+        # self.leds[22] = self.color
+        self.leds[34] = self.color
+        self.draw_frame()
+
+    def run(self):
+        self.running = True
+
+        # # self.leds[0] = self.color
+        # self.leds[11] = self.color
+        # # self.leds[22] = self.color
+        # self.leds[34] = self.color
+        # self.draw_frame()
+
+    def show_single_color(self, color=None):
+        if color is None:
+            color = self.color
+        for pos in range(self.num_pixels):
+            self.leds[pos] = color
+        self.draw_frame()
+
+    def set_colors(self, colors):
+        self.leds = colors
+        self.draw_frame()
+
+    def halo_loop(self):
+        while self.running:
+            for pos in range(self.num_pixels):
+                if not self.running:
+                    break
+                self.fade_tail()
+                self.leds[pos] = self.color
+                self.draw_frame()
+                time.sleep(self.delay)
+
+    def stop(self):
+        self.running = False
+        self.clear()
+
+    def clear(self):
+        self.leds = [(0, 0, 0)] * self.num_pixels
+        self.draw_frame()
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-n", type=int, default=50, help="number of LEDs")
+    ap.add_argument("-t", type=int, default=5, help="duration of test")
+    ap.add_argument("--brightness", type=float, default=1.0, help="LED brightness")
+    ap.add_argument("--color", type=float, nargs=3, default=(227, 253, 255), help="LED color (R, G, B)")
+    args = ap.parse_args()
+
+    led = LED(num_pixels=args.n, brightness=args.brightness, color=args.color)
+    # led.show_single_color(color=args.color)
+
+    led.halo_loop()
+
+    time.sleep(args.t)
+    led.stop()
